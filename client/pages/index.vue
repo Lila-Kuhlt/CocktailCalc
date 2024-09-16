b95a0adededa7f2239682117c99f63c20b806b43
EliasSchaut <eschaut@outlook.de>
2024-09-16 16:45:52 +0200

    add examples

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1 class="dark:text-white text-secondary-200">Hello World</h1>
+  <h1 :class="caskldjfh" @click="changeArray">{{ msg }}</h1>
+  <p v-if="show">asdkljfh</p>
+  <ul>
+    <li v-for="item in arr" :key="item">{{ item }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-
+export default defineComponent({
+  setup() {
+    const caskldjfh = 'dark:text-white text-secondary-200';
+    const msg = 'Hello World';
+    const show = false;
+    const arr = ref([1, 2, 3]);
+    return { caskldjfh, msg, show, arr };
+  },
+  methods: {
+    changeArray() {
+      this.arr.push(4);
+    },
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
