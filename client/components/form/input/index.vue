e776207cee849b275e20cfa2c0b6bb169e894199
EliasSchaut <eschaut@outlook.de>
2024-09-16 19:20:49 +0200

    client: make logic to add ingredients button

---
+++ client/components/form/input/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <input
+    class="block w-16 rounded-md border-0 py-1.5 text-lg text-secondary-900 shadow-sm ring-1 ring-inset ring-secondary-300 placeholder:text-secondary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+  />
+</template>
--- client/pages/ingredients.vue
+++ client/pages/ingredients.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="justify-start space-y-4 p-6">
     <h1 class="text-3xl font-bold">Zutaten</h1>
-    <Bigbutton>Neue Zutat</Bigbutton>
+    <Bigbutton @click="$refs.add_modal.show()">Neue Zutat</Bigbutton>
     <div class="grid grid-cols-4 gap-6">
       <CardIngredient
         v-for="ingredient in ingredients"
@@ -13,6 +13,22 @@
       />
     </div>
   </section>
+  <Modal @submit="add_ingredient()" ref="add_modal">
+    <form
+      ref="add_ingredient_form"
+      class="flex flex-col justify-between space-y-4"
+    >
+      <div>
+        <div class="font-semibold">Neue&nbsp;Zutat</div>
+        <FormInput class="w-full" type="text" name="name" />
+      </div>
+
+      <div>
+        <div class="font-semibold">Preis</div>
+        <FormInputFloat class="w-full" name="price" />
+      </div>
+    </form>
+  </Modal>
 </template>
 
 <script lang="ts">
@@ -27,6 +43,15 @@ export default defineComponent({
     };
   },
   methods: {
+    add_ingredient() {
+      console.log(this.$refs.add_ingredient_form);
+      if (!this.$refs.add_ingredient_form.reportValidity()) return;
+      const form_data = new FormData(this.$refs.add_ingredient_form);
+      const name = form_data.get('name') as string;
+      const price = parseFloat(form_data.get('price') as string);
+      console.log(name, price);
+      this.ingredients.push({ name, price });
+    },
     update_ingredient(name: string, price: number) {
       const ingredient = this.ingredients.find((i) => i.name === name);
       if (!ingredient) return;
