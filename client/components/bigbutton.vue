95781e52525845b51899bdb9ef81ae3c162342db
EliasSchaut <eschaut@outlook.de>
2024-09-16 18:50:10 +0200

    client: add ingredients page

---
+++ client/components/bigbutton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="rounded-md bg-primary-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+  >
+    <slot />
+  </button>
+</template>
---
+++ client/pages/ingredients.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="justify-start space-y-4 p-6">
+    <h1 class="text-3xl font-bold">Zutaten</h1>
+    <Bigbutton>Neue Zutat</Bigbutton>
+    <div class="grid grid-cols-4 gap-6">
+      <CardIngredient
+        v-for="ingredient in ingredients"
+        :key="ingredient.name"
+        :name="ingredient.name"
+        :price="ingredient.price"
+        @update="update_ingredient"
+        @delete="delete_ingredient"
+      />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    return {
+      ingredients: ref([
+        { name: 'Tomate', price: 0.5 },
+        { name: 'Käse', price: 1.5 },
+        { name: 'Salami', price: 2.5 },
+      ]),
+    };
+  },
+  methods: {
+    update_ingredient(name: string, price: number) {
+      const ingredient = this.ingredients.find((i) => i.name === name);
+      if (!ingredient) return;
+      ingredient.price = price;
+    },
+    delete_ingredient(name: string) {
+      this.ingredients = this.ingredients.filter((i) => i.name !== name);
+    },
+  },
+});
+</script>
