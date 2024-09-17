7e4269ea121639447c3179ff6dada3a9fd23e018
EliasSchaut <eschaut@outlook.de>
2024-09-17 11:13:07 +0200

    client: make ingredients deletable and updateable

--- client/components/modal/ingredient.vue
+++ client/components/modal/ingredient.vue
@@ -1,18 +1,18 @@
 <template>
-  <Modal @submit="add_ingredient" ref="modal">
+  <Modal @submit="upsert_ingredient" ref="modal">
     <form
       ref="add_ingredient_form"
       class="flex flex-col justify-between space-y-4"
     >
       <div>
         <div class="font-semibold">Neue&nbsp;Zutat</div>
-        <FormInput class="w-full" type="text" name="name" />
+        <FormInput class="w-full" type="text" name="name" required />
       </div>
 
       <div>
         <div class="font-semibold">Preis</div>
         <div>
-          <FormInputPrice class="w-full" name="price" />
+          <FormInputPrice class="w-full" name="price" required />
         </div>
       </div>
     </form>
@@ -20,21 +20,22 @@
 </template>
 
 <script lang="ts">
-import { call_api, Method } from '~/util/api';
+import { call_ingredient_upsert } from '~/utils/api';
 
 export default defineComponent({
-  emits: ['added_ingredient'],
+  emits: ['upserted_ingredient'],
   methods: {
     show() {
       this.$refs.modal.show();
     },
-    add_ingredient() {
+    upsert_ingredient() {
       if (!this.$refs.add_ingredient_form.reportValidity()) return;
       const form_data = new FormData(this.$refs.add_ingredient_form);
       const name = form_data.get('name') as string;
       const price = parseFloat(form_data.get('price') as string);
-      call_api(Method.POST, '/ingredient', { name, price }).then(() => {
-        this.$emit('added_ingredient', { name, price });
+
+      call_ingredient_upsert(name, price).then(() => {
+        this.$emit('upserted_ingredient', name, price);
         this.$refs.modal.hide();
       });
     },
