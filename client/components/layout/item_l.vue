a1dd226d3806e0f31b8975eef0c3c04bfa494b46
EliasSchaut <eschaut@outlook.de>
2024-09-22 18:39:06 +0200

    client: include sum in recipes and events

---
+++ client/components/layout/item_l.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center gap-x-2" :title="amount">
+    <span>{{ cl }}</span>
+    <span class="font-semibold">L</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  amount: {
+    type: String,
+    required: true,
+  },
+});
+</script>
